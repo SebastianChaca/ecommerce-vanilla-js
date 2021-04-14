746e73b7fd6fa913897468b0c54df4c375f77964
SebastianChaca <sebastian.dalinger@gmail.com>
2021-04-14 09:22:45 -0300

    btn more terminado mas reponsive

--- MainPage/mainpage.js
+++ MainPage/mainpage.js
@@ -1,5 +1,37 @@
 
 
+function createCard(product, gallery){
+  let divContainer = document.createElement('div');
+  divContainer.className = 'content';    
+  divContainer.innerHTML = `
+  
+  <img src=${product.image[0].url} />
+  <h3>${product.title}</h3>
+  <p>${product.shortDescription}</p>
+  <div class='star__container'>
+      <img  src='/Img/ratestar${product.rate}.png'>
+  </div>
+  <hr />
+  <div class="price_container">    
+    <h6 class=${
+      product.descuento ? 'price' : 'offPrice'
+    }>$${product.price * 0.8}</h6>
+    <h6 class=${product.descuento  ? 'crossedPrice' : 'price'}>$${
+      product.price
+    }</h6>
+  </div>
+  <div class="btns__card">
+    <button class="btn__cart">
+      <img src="/Img/miniShopIcon.png" alt="" /> <p>AGREGAR</p> 
+    </button>
+    <button class="btn__detail">
+      <img src="/Img/infoIcon.png" alt="" /><p>DETALLE</p>
+    </button>
+  </div>
+  `; 
+  gallery.appendChild(divContainer);
+  
+}
 function loadingStart() {
   let loading = document.getElementById('loading');
   loading.className = 'loading';
@@ -34,95 +66,89 @@ function filterArray(products, type) {
     return newArray;
   } else if (type === 'novedad') {
     const newArray = products.filter((product) => product.novedad === true);
-    console.log('novedad' + newArray)
+    
     return newArray;
   } else {
     const newArray = products.filter((product) => product.categoria === type);
     return newArray;
   }
 }
-function showMore(){
+let open=false
+function showMore(products, card, id){
   
-  let show=document.getElementById('show')
-    if (show.className=== 'showNone'){
-      show.className='showMore'
-    }else{
-      show.className='showNone'
-    }
+ 
+  if (!open){
+    let titleContainer= document.getElementById('mostrarMas__Title')
+    let imgContainer=document.getElementById(id +1)
+    imgContainer.src='/Img/arrowUp.png'
+    titleContainer.innerHTML='Mostrar Menos'
+    products.slice(4,8).forEach((product) => {   
+      createCard(product, card)
+    });
+    open=true
+  }else{
+    let titleContainer= document.getElementById('mostrarMas__Title')
+    let imgContainer=document.getElementById(id + 1)
+    imgContainer.src='/Img/arrowDown.png'
+    titleContainer.innerHTML='Mostrar Mas'
+    let cardsContainer=document.getElementById(id).getElementsByClassName("content")    
+    while (cardsContainer.length > 0) {
+      cardsContainer[0].parentNode.removeChild(cardsContainer[0]);
+    }    
+    products.slice(0,4).forEach((product) => {   
+      createCard(product, card)
+    });
+    open=false
+    
+  }
   
 }
-function createProduct(products, id, titleContent) {
-  let container = document.querySelector('.container');
-  let galleryContainer = document.createElement('div');
 
-  let gallery = document.createElement('div');
-  gallery.id = id;
-  gallery.className = 'gallery';
+function createProduct(products, id,titleContent) {
+  let container = document.querySelector('.container');
+  let cardContainer = document.createElement('div');
+  cardContainer.id=id
+  let card = document.createElement('div');  
+  card.className = 'gallery';
 
   let title = document.createElement('div');
   title.className = 'titulo';
-  title.innerHTML = `
-  <h2>${titleContent}</h2>
-  `;
+  title.innerHTML = `<h2>${titleContent}</h2>`;
  
   let seeMoreBtn = document.createElement('div');
   seeMoreBtn.className = 'btn__container';
   let btnMore = document.createElement('button');
-  btnMore.onclick=()=>{ showMore()}
+  btnMore.onclick=()=> showMore(products, card, id)
+  let imgId=id + 1
   btnMore.innerHTML = `
-  <h4>Mostrar más...</h4>
-  <img src="/Img/arrowDown.png" alt="" />
+  <h4 id='mostrarMas__Title'>Mostrar más...</h4>
+  <img id=${imgId} src="/Img/arrowDown.png" alt="" />
   `;
   seeMoreBtn.appendChild(btnMore);
 
   
-  products.forEach((product, index) => {
-    const priceDiscount = product.price * 0.8;
-    let divContainer = document.createElement('div');
-    divContainer.className = `${index >3 ? 'content showNone':'content'}`;
-    divContainer.id=`${index >3 ? 'show':''}`;
-    divContainer.innerHTML = `
-    <img src=${product.image[0].url} />
-    <h3>${product.title}</h3>
-    <p>${product.shortDescription}</p>
-    <hr />
-    <div class="price_container">    
-    <h6 class=${
-      id === 'product_discount' ? 'price' : 'offPrice'
-    }>$${priceDiscount}</h6>
-    <h6 class=${id === 'product_discount' ? 'crossedPrice' : 'price'}>$${
-      product.price
-    }</h6>
-    </div>
-    <div class="btns__card">
-    <button class="btn__cart">
-      <img src="/Img/miniShopIcon.png" alt="" /> <p>AGREGAR</p> 
-    </button>
-    <button class="btn__detail">
-      <img src="/Img/infoIcon.png" alt="" /><p>DETALLE</p>
-    </button>
-  </div>
-    `;
-
-    gallery.appendChild(divContainer);
+  products.slice(0,4).forEach((product) => {   
+    createCard(product, card, id)
+    
+    
   });
-  galleryContainer.appendChild(title);
-  galleryContainer.appendChild(gallery);
-  galleryContainer.appendChild(seeMoreBtn);
-  container.appendChild(galleryContainer);
+  cardContainer.appendChild(title);
+  cardContainer.appendChild(card);
+  cardContainer.appendChild(seeMoreBtn);
+  container.appendChild(cardContainer);
 }
 function createSections(r) {
   loadingFinish();
   createProduct(
     filterArray(r, 'descuento'),
-    'product_discount',
+    'product__descuento',
     'Productos en descuento'
   ),
-  createProduct(filterArray(r, 'novedad'), 'product_novedad', 'Novedades');
-  createProduct(filterArray(r, 'Whisky'), 'product_category', 'Whikys');
-  createProduct(filterArray(r, 'Puro'), 'product_puros', 'Puros');
+  createProduct(filterArray(r, 'novedad'),'product__novedad', 'Novedades');
+  createProduct(filterArray(r, 'Whisky'),  'product__whiskys','Whikys');
+  createProduct(filterArray(r, 'Puro'), 'product__puros','Puros');
 }
-getProducts().then((r) => console.log(r));
+
 getProducts().then((r) => createSections(r));
 
 
