a343b61274a0c940693f9a41bb7c0ad74688b51a
Sebastian Chaca <sebastian.dalinger@gmail.com>
2021-04-30 23:57:59 -0300

    mas cosas

--- dashboard/updateproduct.js
+++ dashboard/updateproduct.js
@@ -30,7 +30,7 @@ function stopLoading(){
 }
 
 async function updateProduct(data){
-  starLoading()
+ 
   
  try {
   const response = await fetch(`http://localhost:1337/products/${selectedId}`,{
@@ -49,6 +49,18 @@ async function updateProduct(data){
  }
 }
 
+async function getImg(data){
+  starLoading() 
+  try {
+    const response= await fetch('https://api.cloudinary.com/v1_1/dxexw8kqg/image/upload',{
+      method:'post',
+      body: data
+    })
+    return response.json()
+  } catch (error) {
+    console.log(error)
+  }
+}
 
 const productosStorage= JSON.parse(localStorage.getItem('productos-api'))
 const selectedId=JSON.parse(localStorage.getItem('current-id'))
@@ -77,6 +89,25 @@ img.src= selectedProduct.image ? selectedProduct.image.url : selectedProduct.ima
 novedad.value=selectedProduct.novedad
 descuento.value=selectedProduct.descuento
 
+const idFile =document.getElementById('file')
+const formData= new FormData()
+
+idFile.addEventListener('change', (e)=>{
+  formData.append('file', e.target.files[0])
+  formData.append('upload_preset', 'yetyq6lc')
+  const reader = new FileReader();
+  reader.addEventListener("load", function () {
+    
+    img.src = reader.result;
+  }, false);
+
+  if (file) {
+    reader.readAsDataURL(e.target.files[0]);
+  }
+  
+})
+
+
 novedad.addEventListener('click',()=>{
  novedad.checked ? novedad.value = true : novedad.value=false
  
@@ -88,19 +119,22 @@ descuento.addEventListener('click',()=>{
 document.querySelector('#form').addEventListener('submit', (e)=>{
  
   e.preventDefault()
-  const producto={
-    title: nombre.value,
-    price: precio.value,
-    description: descripcion.value,
-    stock: stock.value,
-    rate: rate.value,
-    categoria: categoria.value,
-    shortDescrition: shortDescription.value,
-    novedad: novedad.value,
-    descuento: descuento.value,}
-  
- 
-  updateProduct(producto).then(()=>{
+  getImg(formData).then(r =>{
+    const producto={
+     title: nombre.value,
+     price: precio.value,
+     description: descripcion.value,
+     stock: stock.value,
+     rate: rate.value,
+     categoria: categoria.value,
+     shortDescription: shortDescription.value,
+     novedad: novedad.value,
+     descuento: descuento.value,
+     imageFromDash: r.url,
+     image:''
+   }     
+    return producto    
+  }).then((producto)=> updateProduct(producto)).then(()=>{
     // nombre.value=''
     // precio.value=''
     // descripcion.value=''
