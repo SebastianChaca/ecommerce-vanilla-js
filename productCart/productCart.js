8daf728c5da0b97b40d45d546e2bf506340ae58b
Sebastian Chaca <sebastian.dalinger@gmail.com>
2021-05-05 10:07:18 -0300

    delete product item logic and check for empty cart array validation

--- productCart/productCart.js
+++ productCart/productCart.js
@@ -1,8 +1,20 @@
 let productMainContainer=document.getElementById('cart_main_container')
-const cartStorage=localStorage.getItem('cart')? JSON.parse(localStorage.getItem('cart')):[]
-
-function delteProduct(id){
-  console.log(id)
+let cartStorage=localStorage.getItem('cart')? JSON.parse(localStorage.getItem('cart')):[]
+let emptyCartElement=document.getElementById('cart_empty')
+let cartDetailElement=document.getElementById('cart_detail')
+let mainContainerELement=document.getElementById('main_container')
+function deleteProduct(id){ 
+  cartStorage.forEach( (p, index )=> {
+    if(p.id === id){
+      cartStorage.splice(index, 1)
+    }
+  })
+  localStorage.setItem('cart', JSON.stringify(cartStorage))
+  let cardsContainer=document.getElementById('cart_main_container').getElementsByClassName("cart_container")  
+  while (cardsContainer.length > 0) {
+    cardsContainer[0].parentNode.removeChild(cardsContainer[0]);
+  }
+  createShoppingCart(cartStorage)  
 }
 
 function createCart(data){
@@ -34,11 +46,20 @@ function createCart(data){
  `
   productMainContainer.appendChild(cartContainer)
 }
+function checkEmptyCart(){
+  emptyCartElement.style.display='block'
+  cartDetailElement.style.display='none'
+  mainContainerELement.style.display='none'
+  
+}
 function createShoppingCart(data){
-  data.forEach(product =>{
-    createCart(product)
-  })
+  if(cartStorage.length > 0){
+    data.forEach(product =>{
+      createCart(product)
+    })
+  }else{
+    checkEmptyCart()
+  }
 }
 document.addEventListener('DOMContentLoaded', createShoppingCart(cartStorage))
 
-console.log(cartStorage)
